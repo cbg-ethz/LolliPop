77a8b9ade5eef79e673002d910f9ee5b838aef89
Ivan Blagoev Topolsky <ivan.topolsky@sib.swiss>
2024-07-04 00:02:33 +0200

    [fix] FutureWarning: Downcasting behavior
    - Pandas 3.0 will not auto change type from objects to e.g. numerical
      use infer_types

--- lollipop/preprocessors.py
+++ lollipop/preprocessors.py
@@ -97,7 +97,7 @@ class DataPreprocesser:
         # self.df_tally = self
         self.df_tally = self.df_tally.replace(
             ["extra", "mut", "shared", "revert", "subset"], 1
-        )
+        ).infer_objects()
 
         # remove uninformative mutations
         variants_columns = list(set(variants_list) & set(self.df_tally.columns))
