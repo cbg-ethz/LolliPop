86d1b3d53e845ca64b800a0d652e842f2c67cd55
Gordon J. Köhn <gordon.koehn@dbsse.ethz.ch>
2024-12-19 10:08:40 +0100

    add tests for sinlge location

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,7 +1,11 @@
 import subprocess
 
 
-def test_workflow():
+def test_workflow_multiple_locs():
+    """
+    Tests workflow with multiple locations,
+    and with multiple cores.
+    """
     # data: its handled with LFS
 
     # dummy run
@@ -22,6 +26,65 @@ def test_workflow():
             "--filters=filters_preprint.yaml",
             "--location=Zürich (ZH)",
             "--location=Genève (GE)",
+            "--seed=42",
+            "preprint/data/tallymut_line_full.tsv.zst",
+        ]
+    )
+
+
+def test_workflow_one_loc():
+    """
+    Tests workflow with only one location, 
+    and with only one core.
+    """
+    # data: its handled with LFS
+
+    # dummy run
+    subprocess.check_call(["lollipop", "--version"])
+
+    # do fast test from preprint
+    subprocess.check_call(
+        [
+            "lollipop",
+            "deconvolute",
+            "--n-cores=2",
+            "--par-bar=1",
+            "--output=test_results.csv",
+            "--out-json=test_results.json",
+            "--fmt-columns",
+            "--variants-config=config_preprint.yaml",
+            "--deconv-config=presets/deconv_linear.yaml",
+            "--filters=filters_preprint.yaml",
+            "--location=Zürich (ZH)",
+            "--seed=42",
+            "preprint/data/tallymut_line_full.tsv.zst",
+        ]
+    )
+
+def test_workflow_one_loc():
+    """
+    Tests workflow with only one location,
+    and with only one core.
+    """
+    # data: its handled with LFS
+
+    # dummy run
+    subprocess.check_call(["lollipop", "--version"])
+
+    # do fast test from preprint
+    subprocess.check_call(
+        [
+            "lollipop",
+            "deconvolute",
+            "--n-cores=1",
+            "--par-bar=1",
+            "--output=test_results.csv",
+            "--out-json=test_results.json",
+            "--fmt-columns",
+            "--variants-config=config_preprint.yaml",
+            "--deconv-config=presets/deconv_linear.yaml",
+            "--filters=filters_preprint.yaml",
+            "--location=Zürich (ZH)",
             "--seed=42",
             "preprint/data/tallymut_line_full.tsv.zst",
         ]
